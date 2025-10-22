ebb0de28fc2c7fe51b7d6c590e616d3e84a777a9
Mumtahina Nur <mumtahinanur9@gmail.com>
2025-10-22 11:41:57 +0600

    complete exercise on loops and array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arg) {
+  return array.filter(val => !arg.includes(val));
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  const cel = ((num - 32) * (5/9));
+  return +cel.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  const far = ((num * 9) / 5) + 32;
+  return +far.toFixed(1);
 };
 
 // Do not edit below this line
