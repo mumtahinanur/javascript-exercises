142cf31c45a786d72ecdc451912b9c6b3a6b37a4
Mumtahina Nur <mumtahinanur9@gmail.com>
2025-10-27 21:12:23 +0600

    complete javascript exercise on objects

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, item) => sum + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((mul, item) => mul * item, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0 || n=== 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
